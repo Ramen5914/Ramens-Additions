4662e5e52b4fb31a78b4708bb8b4f54f51474e99
Omar Rahman <omar+github@ramen5914.net>
2024-09-15 07:34:36 -0400

    Moved EMI things into the compat package

---
+++ src/main/java/net/ramen5914/ramensadditions/compat/emi/ModEmiPlugin.java
@@ -0,0 +1,99 @@
+package net.ramen5914.ramensadditions.compat.emi;
+
+import com.google.common.collect.Lists;
+import dev.emi.emi.api.EmiEntrypoint;
+import dev.emi.emi.api.EmiPlugin;
+import dev.emi.emi.api.EmiRegistry;
+import dev.emi.emi.api.recipe.EmiInfoRecipe;
+import dev.emi.emi.api.recipe.EmiRecipeCategory;
+import dev.emi.emi.api.stack.EmiIngredient;
+import dev.emi.emi.api.stack.EmiStack;
+import net.minecraft.client.Minecraft;
+import net.minecraft.core.Holder;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.ramen5914.ramensadditions.RamensAdditions;
+import net.ramen5914.ramensadditions.block.ModBlocks;
+import net.ramen5914.ramensadditions.compat.emi.recipes.AdvancedGrindingRecipe;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.List;
+
+@EmiEntrypoint
+public class ModEmiPlugin implements EmiPlugin {
+    public static final ResourceLocation WIDGET_SPRITE_SHEET = ResourceLocation.fromNamespaceAndPath(RamensAdditions.MOD_ID, "textures/gui/widgets.png");
+
+    // Advanced Grindstone
+    public static final EmiStack ADVANCED_GRINDSTONE = EmiStack.of(ModBlocks.ADVANCED_GRINDSTONE);
+    public static final EmiRecipeCategory ADVANCED_GRINDING = new EmiRecipeCategory(ResourceLocation.fromNamespaceAndPath(RamensAdditions.MOD_ID, "advanced_grinding"), ADVANCED_GRINDSTONE);
+
+    @Override
+    public void register(EmiRegistry registry) {
+        List<Enchantment> targetedEnchantments = Lists.newArrayList();
+        List<Enchantment> universalEnchantments = Lists.newArrayList();
+        for (Enchantment enchantment : Minecraft.getInstance().level.registryAccess().registryOrThrow(Registries.ENCHANTMENT)) {
+            try {
+                if (enchantment.canEnchant(ItemStack.EMPTY)) {
+                    universalEnchantments.add(enchantment);
+                    continue;
+                }
+            } catch (Throwable ignored) {
+
+            }
+            targetedEnchantments.add(enchantment);
+        }
+
+        // Advanced Grindstone
+        registry.addCategory(ADVANCED_GRINDING);
+        registry.addWorkstation(ADVANCED_GRINDING, ADVANCED_GRINDSTONE);
+        registry.addRecipe(
+                new EmiInfoRecipe(
+                        List.of(EmiIngredient.of(Ingredient.of(ModBlocks.ADVANCED_GRINDSTONE.get()))),
+                        List.of(Component.literal(
+                                "An upgrade of the vanilla grindstone which has the power to remove curses! (at a cost)"
+                        )),
+                        ResourceLocation.fromNamespaceAndPath(RamensAdditions.MOD_ID, "/info_recipes/advanced_grindstone")
+                )
+        );
+
+        for (Item i : BuiltInRegistries.ITEM) {
+            try {
+                ItemStack defaultStack = i.getDefaultInstance();
+                int acceptableEnchantments = 0;
+                for (Enchantment e : targetedEnchantments) {
+                    if (defaultStack.supportsEnchantment(Holder.direct(e))) {
+                        acceptableEnchantments++;
+                    }
+                }
+                if (acceptableEnchantments > 0) {
+                    for (Enchantment e : universalEnchantments) {
+                        if (defaultStack.supportsEnchantment(Holder.direct(e))) {
+                            acceptableEnchantments++;
+                        }
+                    }
+                    String itemNamespace = BuiltInRegistries.ITEM.getKey(i).getNamespace();
+                    String itemPath = BuiltInRegistries.ITEM.getKey(i).getPath();
+                    String id = String.format("/advanced_grindstone/disenchanting/%s/%s", itemNamespace, itemPath);
+
+                    registry.addRecipe(new AdvancedGrindingRecipe(i, ResourceLocation.fromNamespaceAndPath(RamensAdditions.MOD_ID, id)));
+                }
+            } catch (Throwable t) {
+                RamensAdditions.LOGGER.warn("Exception thrown registering enchantment recipes");
+                t.printStackTrace();
+                StringWriter writer = new StringWriter();
+                t.printStackTrace(new PrintWriter(writer, true));
+                String[] strings = writer.getBuffer().toString().split("/");
+                for (String s : strings) {
+                    RamensAdditions.LOGGER.warn(s);
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/net/ramen5914/ramensadditions/compat/emi/recipes/AdvancedGrindingRecipe.java
@@ -0,0 +1,125 @@
+package net.ramen5914.ramensadditions.compat.emi.recipes;
+
+import com.google.common.collect.Lists;
+import dev.emi.emi.api.recipe.EmiRecipe;
+import dev.emi.emi.api.recipe.EmiRecipeCategory;
+import dev.emi.emi.api.stack.EmiIngredient;
+import dev.emi.emi.api.stack.EmiStack;
+import dev.emi.emi.api.widget.WidgetHolder;
+import net.minecraft.client.Minecraft;
+import net.minecraft.core.Holder;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.ramen5914.ramensadditions.ModTags;
+import net.ramen5914.ramensadditions.compat.emi.ModEmiPlugin;
+import net.ramen5914.ramensadditions.gui.screen.AdvancedGrindstoneScreen;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+import java.util.Random;
+
+public class AdvancedGrindingRecipe implements EmiRecipe {
+    private final int uniq = new Random().nextInt();
+    private final Item tool;
+    private final ResourceLocation id;
+
+    public AdvancedGrindingRecipe(Item tool, ResourceLocation id) {
+        this.tool = tool;
+        this.id = id;
+    }
+
+    @Override
+    public EmiRecipeCategory getCategory() {
+        return ModEmiPlugin.ADVANCED_GRINDING;
+    }
+
+    @Override
+    public @Nullable ResourceLocation getId() {
+        return id;
+    }
+
+    @Override
+    public List<EmiIngredient> getInputs() {
+        return List.of(EmiStack.of(tool), EmiIngredient.of(ModTags.Items.DISENCHANTING_CATALYSTS));
+    }
+
+    @Override
+    public List<EmiStack> getOutputs() {
+        return List.of(EmiStack.of(tool));
+    }
+
+    @Override
+    public int getDisplayWidth() {
+        return 154;
+    }
+
+    @Override
+    public int getDisplayHeight() {
+        return 79;
+    }
+
+    @Override
+    public boolean supportsRecipeTree() {
+        return false;
+    }
+
+    @Override
+    public void addWidgets(WidgetHolder widgets) {
+        widgets.addTexture(AdvancedGrindstoneScreen.ADVANCED_GRINDSTONE_BACKGROUND, 0, 0, 154, 79, 11, 15);
+
+        widgets.addGeneratedSlot(r -> getTool(r, true, widgets), uniq, 22, 14).drawBack(false);
+        widgets.addSlot(EmiIngredient.of(ModTags.Items.DISENCHANTING_CATALYSTS), 83, 18).drawBack(false);
+        widgets.addGeneratedSlot(r -> getTool(r, false, widgets), uniq, 132, 18).drawBack(false).recipeContext(this);
+
+        widgets.addText(Component.literal("All"), 5, 66, 0xffffff, true);
+    }
+
+    private EmiStack getTool(Random random, Boolean enchant, WidgetHolder widgets) {
+        ItemStack itemStack = new ItemStack(tool);
+        int enchantments = 1 + Math.max(random.nextInt(5), random.nextInt(3));
+
+        List<Enchantment> list = Lists.newArrayList();
+
+        final Registry<Enchantment> ENCHANTMENT_REGISTRY = Minecraft.getInstance().level.registryAccess().registryOrThrow(Registries.ENCHANTMENT);
+
+        outer:
+        for (int i = 0; i < enchantments; i++) {
+            Enchantment enchantment = getEnchantment(random);
+
+            int maxLvl = enchantment.getMaxLevel();
+            int minLvl = enchantment.getMinLevel();
+
+            int lvl = maxLvl > 0 ? random.nextInt(maxLvl) + 1 : 0;
+
+            if (lvl < minLvl) {
+                lvl = minLvl;
+            }
+
+            for (Enchantment e : list) {
+                if (e == enchantment || !Enchantment.areCompatible(ENCHANTMENT_REGISTRY.wrapAsHolder(e), ENCHANTMENT_REGISTRY.wrapAsHolder(enchantment))) {
+                    continue outer;
+                }
+            }
+            list.add(enchantment);
+
+            if (enchant) {
+                itemStack.enchant(ENCHANTMENT_REGISTRY.wrapAsHolder(enchantment), lvl);
+            }
+        }
+
+        return EmiStack.of(itemStack);
+    }
+
+    private Enchantment getEnchantment(Random random) {
+        final Registry<Enchantment> ENCHANTMENT_REGISTRY = Minecraft.getInstance().level.registryAccess().registryOrThrow(Registries.ENCHANTMENT);
+
+        List<Enchantment> enchantments = ENCHANTMENT_REGISTRY.stream().filter(i -> tool.getDefaultInstance().supportsEnchantment(Holder.direct(i))).toList();
+        int enchantment = random.nextInt(enchantments.size());
+        return enchantments.get(enchantment);
+    }
+}
