4a1eeef9fcb35abf46ee68fb6c962e3affd41da1
Omar Rahman <omar+github@ramen5914.net>
2024-09-16 16:29:34 -0700

    Renamed an event

--- src/main/java/net/ramen5914/ramensadditions/event/ModBusEvents.java
+++ src/main/java/net/ramen5914/ramensadditions/event/ModBusEvents.java
@@ -3,6 +3,8 @@ package net.ramen5914.ramensadditions.event;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.RegisterMenuScreensEvent;
+import net.neoforged.neoforge.network.event.RegisterPayloadHandlersEvent;
+import net.neoforged.neoforge.network.registration.PayloadRegistrar;
 import net.ramen5914.ramensadditions.RamensAdditions;
 import net.ramen5914.ramensadditions.gui.menu.ModMenuTypes;
 import net.ramen5914.ramensadditions.gui.screen.AdvancedGrindstoneScreen;
@@ -10,7 +12,7 @@ import net.ramen5914.ramensadditions.gui.screen.AdvancedGrindstoneScreen;
 @EventBusSubscriber(modid = RamensAdditions.MOD_ID, bus = EventBusSubscriber.Bus.MOD)
 public class ModBusEvents {
     @SubscribeEvent
-    public static void registerScreens(RegisterMenuScreensEvent event) {
+    public static void registerMenuScreens(RegisterMenuScreensEvent event) {
         event.register(ModMenuTypes.ADVANCED_GRINDSTONE.get(), AdvancedGrindstoneScreen::new);
     }
 }
