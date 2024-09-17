e987e0ef1b94ef50e07c7b4e83603592ddc7935c
Omar Rahman <omar+github@ramen5914.net>
2024-09-17 07:02:49 -0700

    Added info recipes and translations

--- src/main/java/net/ramen5914/ramensadditions/compat/jei/ModJEIPlugin.java
+++ src/main/java/net/ramen5914/ramensadditions/compat/jei/ModJEIPlugin.java
@@ -2,11 +2,16 @@ package net.ramen5914.ramensadditions.compat.jei;
 
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.recipe.RecipeType;
+import mezz.jei.api.recipe.vanilla.IJeiIngredientInfoRecipe;
 import mezz.jei.api.registration.IGuiHandlerRegistration;
 import mezz.jei.api.registration.IRecipeCategoryRegistration;
 import mezz.jei.api.registration.IRecipeRegistration;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
 import net.ramen5914.ramensadditions.RamensAdditions;
+import net.ramen5914.ramensadditions.block.ModBlocks;
 
 @JeiPlugin
 public class ModJEIPlugin implements IModPlugin {
@@ -17,16 +22,16 @@ public class ModJEIPlugin implements IModPlugin {
 
     @Override
     public void registerCategories(IRecipeCategoryRegistration registration) {
-        IModPlugin.super.registerCategories(registration);
+
     }
 
     @Override
     public void registerGuiHandlers(IGuiHandlerRegistration registration) {
-        IModPlugin.super.registerGuiHandlers(registration);
+
     }
 
     @Override
     public void registerRecipes(IRecipeRegistration registration) {
-        IModPlugin.super.registerRecipes(registration);
+        registration.addItemStackInfo(new ItemStack(ModBlocks.ADVANCED_GRINDSTONE.get()), Component.translatable(String.format("inforecipe.%s.advanced_grindstone", RamensAdditions.MOD_ID)));
     }
 }
